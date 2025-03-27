71fc8f7bee9a10124199d39f4951d44efb0f2d38
Ivan <ivan_navarro1@outlook.com>
2025-03-27 08:07:23 -0600

    cambio

--- backend/Controllers/app.py
+++ backend/Controllers/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
+#REGLAS PARA EL DIAGNOSTICO DE FALLAS, esto debe ser una base de datos
 REGLAS = [
     {
         "sintomas": ["Ruido Extraño"],
